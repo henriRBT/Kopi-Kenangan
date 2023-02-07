4427be6d4f45dca759736e0bc713802b2e99eddf
Henri <henritom1301@gmail.com>
2023-02-07 21:38:52 +0700

    website kopi kenangan sederhana

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hambuger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik diluar side bar menghilangkan nav
+
+const hambuger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hambuger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+}) 
